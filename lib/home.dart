33678a5e6427421c8d1b2ab24943e664011fb3ec
あかるいちゃん <zhuw160@gmail.com>
2022-11-23 23:02:32 +0800

    delete useless

--- lib/home.dart
+++ lib/home.dart
@@ -26,35 +26,3 @@ class _HomePageState extends State<HomePage> {
     );
   }
 }
-
-
-class WillPopScopeTestRoute extends StatefulWidget {
-  const WillPopScopeTestRoute({super.key});
-
-  @override
-  WillPopScopeTestRouteState createState() {
-    return WillPopScopeTestRouteState();
-  }
-}
-
-class WillPopScopeTestRouteState extends State<WillPopScopeTestRoute> {
-  DateTime? _lastPressedAt;
-
-  @override
-  Widget build(BuildContext context) {
-    return WillPopScope(
-      onWillPop: () async {
-        if (_lastPressedAt == null ||
-            DateTime.now().difference(_lastPressedAt!) > const Duration(seconds: 2)) {
-          _lastPressedAt = DateTime.now();
-          return false;
-        }
-        return true;
-      },
-      child: Container(
-        alignment: Alignment.center,
-        child: const Text("1秒内连续按两次返回键退出"),
-      ),
-    );
-  }
-}
